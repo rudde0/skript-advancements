f39ee4e2dd24520feeed2dc2d276450b73e9b3a0
hotpocket184 <80357042+hotpocket184@users.noreply.github.com>
2022-06-24 22:34:16 -0400

    Added Advancement Message

---
+++ src/main/java/me/hotpocket/skriptadvancements/elements/expressions/ExprAdvancementMessage.java
@@ -0,0 +1,55 @@
+package me.hotpocket.skriptadvancements.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.expressions.base.EventValueExpression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.util.coll.CollectionUtils;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.jetbrains.annotations.Nullable;
+
+public class ExprAdvancementMessage extends EventValueExpression<String> {
+
+    static {
+        Skript.registerExpression(ExprAdvancementMessage.class, String.class, ExpressionType.SIMPLE,
+                "[the] advancement message",
+                "advancement's message",
+                "[the] message of [the] advancement");
+    }
+
+    public ExprAdvancementMessage() {
+        super(String.class);
+    }
+
+    @Override
+    public @Nullable Class<?>[] acceptChange(Changer.ChangeMode mode) {
+        return switch (mode) {
+            case SET -> CollectionUtils.array(String.class);
+            default -> null;
+        };
+    }
+
+    @Override
+    public void change(Event e, @Nullable Object[] delta, Changer.ChangeMode mode) {
+        PlayerAdvancementDoneEvent event = (PlayerAdvancementDoneEvent) e;
+        assert delta[0] != null;
+        switch (mode) {
+            case SET -> event.message(Component.text((String) delta[0]));
+        }
+    }
+
+    @Override
+    @Nullable
+    protected String[] get(Event e) {
+        PlayerAdvancementDoneEvent event = (PlayerAdvancementDoneEvent) e;
+        return new String[] { Bukkit.getUnsafe().legacyComponentSerializer().serialize(event.message()) };
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "the advancement message";
+    }
+}
