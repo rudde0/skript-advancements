f971680a6e26b3d3c43ea887590e983688ae5be2
hotpocket184 <80357042+hotpocket184@users.noreply.github.com>
2022-12-23 15:34:55 -0500

    Player Event Value

--- src/main/java/me/hotpocket/skriptadvancements/elements/events/EvtAdvancementProgression.java
+++ src/main/java/me/hotpocket/skriptadvancements/elements/events/EvtAdvancementProgression.java
@@ -6,6 +6,8 @@ import ch.njol.skript.registrations.EventValues;
 import ch.njol.skript.util.Getter;
 import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
 import com.fren_gor.ultimateAdvancementAPI.events.advancement.AdvancementProgressionUpdateEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
 public class EvtAdvancementProgression extends SimpleEvent {
@@ -15,6 +17,14 @@ public class EvtAdvancementProgression extends SimpleEvent {
                 .since("1.4")
                 .description("Called when an advancement's progression is changed for a player.")
                 .examples("on advancement progression:");
+        EventValues.registerEventValue(AdvancementProgressionUpdateEvent.class, Player.class, new Getter<Player, AdvancementProgressionUpdateEvent>() {
+            @Override
+            @Nullable
+            public Player get(AdvancementProgressionUpdateEvent e) {
+                return Bukkit.getPlayer(e.getTeamProgression().getAMember());
+            }
+
+        }, 0);
         EventValues.registerEventValue(AdvancementProgressionUpdateEvent.class, Advancement.class, new Getter<Advancement, AdvancementProgressionUpdateEvent>() {
             @Override
             @Nullable
