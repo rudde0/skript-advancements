7758c5d7fe8827af0635b190c3e0569277302db7
hotpocket184 <80357042+hotpocket184@users.noreply.github.com>
2022-12-26 16:27:34 -0500

    Update checker

--- src/main/java/me/hotpocket/skriptadvancements/SkriptAdvancements.java
+++ src/main/java/me/hotpocket/skriptadvancements/SkriptAdvancements.java
@@ -10,10 +10,17 @@ import me.hotpocket.skriptadvancements.bstats.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
 
 public final class SkriptAdvancements extends JavaPlugin implements Listener {
 
@@ -21,6 +28,8 @@ public final class SkriptAdvancements extends JavaPlugin implements Listener {
     public static AdvancementMain main;
     private UltimateAdvancementAPI api;
     private SkriptAddon addon;
+    private static List<UUID> joined = new ArrayList<>();
+    private static boolean updated = true;
 
     public static SkriptAdvancements getInstance() {
         return instance;
@@ -53,6 +62,15 @@ public final class SkriptAdvancements extends JavaPlugin implements Listener {
             var2.printStackTrace();
         }
 
+        log("§7[§bSkript-Advancements§7] §fChecking for updates...");
+        if (getDescription().getVersion().equals(getVersion())) {
+            log("§7[§bSkript-Advancements§7] §aNo updates found!");
+            updated = true;
+        }
+        else {
+            log("§7[§bSkript-Advancements§7] §cYou are running an §noutdated version§r §cof skript-advancements!");
+            updated = false;
+        }
     }
 
     @Override
@@ -63,6 +81,31 @@ public final class SkriptAdvancements extends JavaPlugin implements Listener {
     @EventHandler
     private void onPlayerLoadingCompleted(PlayerLoadingCompletedEvent event) {
         for (AdvancementTab tab : api.getTabs())
-            tab.updateEveryAdvancement(event.getPlayer());
+            tab.updateAdvancementsToTeam(event.getPlayer());
+    }
+
+    @EventHandler
+    private void onJoin(PlayerJoinEvent event) {
+        if (!updated) {
+            if (event.getPlayer().isOp() && !joined.contains(event.getPlayer().getUniqueId())) {
+                event.getPlayer().sendMessage("§7[§bSkript-Advancements§7] §cYou are running an §noutdated version§r §cof skript-advancements!");
+                joined.add(event.getPlayer().getUniqueId());
+            }
+        }
+    }
+
+    private String getVersion() {
+        try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=96702").openStream(); Scanner scanner = new Scanner(inputStream)) {
+            if (scanner.hasNext()) {
+                return scanner.next();
+            }
+        } catch (IOException exception) {
+            this.getLogger().info("Unable to check for updates: " + exception.getMessage());
+        }
+        return "";
+    }
+
+    private void log(String text) {
+        getServer().getConsoleSender().sendMessage(text);
     }
 }
