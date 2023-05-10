5d616e7bd76b8ad137e20092ccb1e9138c9d70e4
hotpocket184 <80357042+hotpocket184@users.noreply.github.com>
2023-05-09 20:47:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,30 +21,24 @@ function loadSyntax() {
         var count = 0;
         jsonData = data;
         for (var i = 0; i < data.length; i++) {
-            if (data[i].creator == "2270") {
+            if (data[i].addon.name == "skript-advancements") {
                 let typeTitle = data[i].syntax_type.charAt(0).toUpperCase() + data[i].syntax_type.slice(1);
                 let colorForBackground = "rgb(221, 221, 221)"
                 let colorForSyntax = "rgb(199, 199, 199)"
                 if (typeTitle == "Type") {
                   colorForBackground = "orange"
-                  colorForSyntax = "rgb(228, 148, 0)"
                 } else if (typeTitle == "Expression") {
-                  colorForBackground = "rgb(55, 204, 122)"
-                  colorForSyntax = "rgb(51, 184, 111)"
+                  colorForBackground = "mediumseagreen"
                 } else if (typeTitle == "Effect") {
-                  colorForBackground = "rgb(0, 174, 255)"
-                  colorForSyntax = "rgb(0, 156, 228)"
+                  colorForBackground = "dodgerblue"
                 } else if (typeTitle == "Condition") {
                   colorForBackground = "rgb(223, 75, 75)"
-                  colorForSyntax = "rgb(201, 67, 67)"
                 } else if (typeTitle == "Section") {
-                  colorForBackground = "rgb(55, 209, 183)"
-                  colorForSyntax = "rgb(51, 192, 169)"
+                  colorForBackground = "lightseagreen"
                 } else if (typeTitle == "Event") {
-                  colorForBackground = "rgb(224, 95, 214)"
-                  colorForSyntax = "rgb(201, 87, 191)"
+                  colorForBackground = "mediumorchid"
                 }
-                document.getElementById("elements").insertAdjacentHTML("beforeend", "<div id='content' style='background-color:" + colorForBackground + ";'><h2 id='title'>" + typeTitle + " | " + data[i].title + "</h2><p id='description'>" + data[i].description + "</p><p id='syntax' style='background-color:" + colorForSyntax + ";'>" + data[i].syntax_pattern.replace("\n", "<br>") + "</p></div>")
+                document.getElementById("elements").insertAdjacentHTML("beforeend", "<div id='content' style='border-left: 7px solid " + colorForBackground + ";'><h3 id='title'>" + typeTitle + " | " + data[i].title + "</h3><p id='syntax' style='background-color: rgb(235, 235, 235);'>" + data[i].syntax_pattern.replace("\n", "<br>") + "</p><p id='description'>" + data[i].description + "</p></div>")
             }
         }
       });
